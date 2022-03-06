e4cc9422ae06b9b3fddd4e74b656e8908cf49b3c
Baptiste Auffeuvre <baptiste.auffeuvre@beslogic.com>
2022-03-06 14:48:41 -0500

    Added header + main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RecipeDetailComponent } from './components/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-book/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './components/shopping list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping list/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    HeaderComponent,
+    RecipeDetailComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
